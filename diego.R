7d8dcd8f28621280aa0fd657f2e1f5816616f7be
diegomunoz2333 <diegofmup23@gmail.com>
2025-10-04 14:19:19 -0500

    actualizacion base de datos educacion solo una vaariable importante se agrega una base de datos

--- diego.R
+++ diego.R
@@ -1,1 +1,16 @@
-#diego #diego
+#diego #diego   
+#analisis de variables
+#primero pues la y= ingreso de la persona ()
+#suposicion de primeras x
+#X=sexo(), niveleducativo(P8587), edad(), horas_semana()
+#estrato()
+library(readr)
+library(tidyverse)
+library(dplyr)
+ECV <- read_delim("Educacion.CSV", 
+                        delim = ";", escape_double = FALSE, trim_ws = TRUE)
+View(ECV)
+
+#filtrare para ver la importancia de las variables
+ECV=ECV %>% select(P8587)
+view(ECV)
