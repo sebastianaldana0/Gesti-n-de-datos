939b68464f5e48b503d048f4efd57d5915ee2f8a
sebastianaldana0 <joan.aguirre@correounivalle.edu.co>
2025-10-04 21:36:24 -0500

    k

--- Sebastian.R
+++ Sebastian.R
@@ -1,3 +1,16 @@
 #Pruebas de Sebastian
 
 #universidad del valle
+%>% select(DIRECTORIO,Municipio,Estrato,`Ingreso del hogar`,
+           Arriendo_estimacion,`Cantidad de personas en el hogar`,
+           Sexo,Edad,`Ultimo grado alcanzado`,
+           ,sastifacion,PERCAPITA)
+
+Modelo_final=lm(`Ingreso del hogar`~Sexo+Tiempo_trabajado+`Ultimo grado alcanzado`+
+                  `Cantidad de personas en el hogar`+Edad,Base_datos)
+
+summary(Modelo_final)
+plot(Modelo_final,which=1)
+plot(Modelo_final,which=2)
+plot(Modelo_final,which=3)
+plot(Modelo_final,which=5)
