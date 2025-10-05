922e16ed684128561f577148887d297400241380
sebastianaldana0 <joan.aguirre@correounivalle.edu.co>
2025-10-05 10:01:02 -0500

    ENA

--- ENA.R
+++ ENA.R
@@ -24,12 +24,40 @@ lote=read.csv("Lotes.csv",sep="|") %>%
 
 cultivo=read.csv("cultivos.csv",sep="|") %>%
   select(KEYPPAL_CU,KEYPPAL_L,CUL_TIPO,SIEMBRA_MES,
-         SIEMBRA_ANIO,COSECHA_MES,EFEC_CLIM_PER_COSECHA,
-         AR_SEMOPLAN,REND_OP_2019) 
+         SIEMBRA_ANIO,COSECHA_MES,
+         AR_SEMOPLAN,REND_OP_2019)
+
+cosecha=read.csv("cosecha.csv",sep="|") %>% 
+  select(KEYPPAL_CU,AR_COS,COSECHA_CANT,REND_DEF,PORC_VENTA,
+         PRECIO_VENUM,PRECIO_X_KILO) %>% 
+  rename(Area_cosecha=2,Cantidad_cosechada=3,Rendimiento=4,"%venta"=5)
 
 #Base de datos final
 
-Base_datos_ENA=suelo %>% inner_join(productor,by="KEYPPAL")
+Base_datos_ENA=suelo %>% inner_join(productor,by="KEYPPAL") %>%
+  inner_join(lote,by="KEYPPAL")
+  
 
-Base_datos_lotes_ENA=lote 
+Base_datos_cosecha_ENA=cultivo %>% 
+  inner_join(cosecha,by="KEYPPAL_CU") %>% inner_join(lote,by="KEYPPAL_L")%>% 
+  group_by(KEYPPAL) %>% 
+  summarise(Ventas=log(sum(PRECIO_X_KILO*Cantidad_cosechada*1000))) %>% 
+  inner_join(suelo,by="KEYPPAL") %>% inner_join(productor,by="KEYPPAL")%>% 
+  filter(!is.na(Ventas))
+
+
+
+
+  select(-KEYPPAL_CU,-KEYPPAL_L,-KEYPPAL) %>%
+  mutate(Ventas=log(PRECIO_X_KILO*Cantidad_cosechada*1000)) 
+#Modelos
+
+Modelo_final=lm(Ventas~Estudios+Sexo+Edad+AR_USUELO,Base_datos_cosecha_ENA)
+cor(Base_datos_cosecha_ENA)
+summary(Modelo_final)
+plot(Modelo_final,which=1)
+plot(Modelo_final,which=2)
+plot(Modelo_final,which=3)
+plot(Modelo_final,which=5)
+#Pruebas modelo
                                      
